11ea8f75fd0ecca5df5f88dee2e893333883971b
Vinicius8706 <76622945+Vinicius8706@users.noreply.github.com>
2021-02-10 13:06:25 -0300

    Create booksByCategory.js

---
+++ booksByCategory.js
@@ -0,0 +1,86 @@
+const booksByCategory = [
+    { 
+        category: "Riqueza",
+        books: [
+            { title: "Os segredos da mente milionária",
+            author: "T, Harv Eker"},
+        { title: "O homem mais rico da Babilônia",
+            author: "George S. Clason"},
+        { title: "Pai rico, pai pobre",
+            author: "Robert T. Kiyosaki e Sharon L. Lechter"},
+        ],
+        },
+        { category: "Inteligência Emocional",
+            books: [
+            { title: "Você é insubstituível",
+        author: "Augusto Cury"},
+        {
+            title: "Ansiedade - Como enfrentar o mal do século",
+            author: "Augusto Cury"},
+        { title: "Os 7 hábitos das pessoas altamente eficazes",
+        author: "Stephen R. Covey"
+        },
+        
+
+    
+    ],
+
+
+
+
+},
+];
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories)
+for(let category of booksByCategory){
+    console.log('Total de livros da categoria: ', category)
+    console.log(category.books.length)
+}
+
+function countAuthors(){
+    let authors = [];
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            // se achou o autor lá dentro
+            if(authors.indexOf(book.author) === -1)
+                authors.push(book.author)
+        }
+    } 
+
+    console.log("Total de autores: ", authors.length)
+}
+
+countAuthors()
+
+function booksOfAugustoCury(){
+    let books = [];
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            // se achou o autor lá dentro
+            if(book.author === 'Augusto Cury'){
+                books.push(book.title)
+            }
+        }
+    } 
+
+    console.log("Livros do autor: ", books)
+}
+
+booksOfAugustoCury();
+
+function booksOfAuthor(author){
+    let books = [];
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            // se achou o autor lá dentro
+            if(book.author === author){
+                books.push(book.title)
+            }
+        }
+    } 
+
+    console.log(`"Livros do autor ${author}: ", ${books.join(", ")}`)
+}
+
+booksOfAuthor('Augusto Cury')
